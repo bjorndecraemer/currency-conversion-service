1ecb293334fc2dfc30e6d00e9cc5708461d2bb58
Bjorn De Craemer <bjorn.decraemer@yahoo.com>
2019-01-26 22:52:47 +0100

    Added Feign Proxy

--- src/main/java/com/bjornspetprojects/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/bjornspetprojects/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package com.bjornspetprojects.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.bjornspetprojects.currencyconversionservice")
 public class CurrencyConversionServiceApplication {
 
     public static void main(String[] args) {
@@ -11,4 +13,3 @@ public class CurrencyConversionServiceApplication {
     }
 
 }
-
--- src/main/java/com/bjornspetprojects/currencyconversionservice/controllers/CurrencyConversionController.java
+++ src/main/java/com/bjornspetprojects/currencyconversionservice/controllers/CurrencyConversionController.java
@@ -1,6 +1,7 @@
 package com.bjornspetprojects.currencyconversionservice.controllers;
 
 import com.bjornspetprojects.currencyconversionservice.beans.CurrencyConversionBean;
+import com.bjornspetprojects.currencyconversionservice.proxy.CurrencyExchangeServiceProxy;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,12 @@ import java.util.Map;
 @RestController
 public class CurrencyConversionController {
 
+    private final CurrencyExchangeServiceProxy currencyExchangeServiceProxy;
+
+    public CurrencyConversionController(CurrencyExchangeServiceProxy currencyExchangeServiceProxy) {
+        this.currencyExchangeServiceProxy = currencyExchangeServiceProxy;
+    }
+
     @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversionBean convertCurrency(@PathVariable String from,
                                                   @PathVariable String to,
@@ -26,6 +33,16 @@ public class CurrencyConversionController {
                 CurrencyConversionBean.class,
                 uriVariables);
         CurrencyConversionBean response = responseEntity.getBody();
+
+        return new CurrencyConversionBean(response.getId(),from,to,response.getConversionMultiple(),quantity, quantity.multiply(response.getConversionMultiple()),response.getPort());
+    }
+
+    @GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from,
+                                                  @PathVariable String to,
+                                                  @PathVariable BigDecimal quantity) {
+
+        CurrencyConversionBean response = currencyExchangeServiceProxy.retrieveExchangeValue(from,to);
 
         return new CurrencyConversionBean(response.getId(),from,to,response.getConversionMultiple(),quantity, quantity.multiply(response.getConversionMultiple()),response.getPort());
     }
